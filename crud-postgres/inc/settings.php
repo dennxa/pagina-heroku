4e98784be2d6626ba7fa5c788096f3e357bf79b6
dennxa <denia.98@hotmail.com>
2021-10-25 11:35:33 -0700

    first commit

---
+++ crud-postgres/inc/settings.php
@@ -0,0 +1,20 @@
+<?php 
+$servername = "localhost";
+$username = "postgres";
+$password = "12345";
+$dbname = "databas";
+$port = 5432;
+
+function validar(){
+    session_start();
+if (empty($_SESSION["nombre"]))
+{
+  echo "Se detecto un acceso ilegal al sistema, su ip esta siendo monitoreada y sera enviada a la policia";
+  ?>
+  <a href="http://localhost/crud-postgres">Sitio de login</a>
+  <?php
+  exit();
+}
+}
+
+?>
