85c48c2b3875d0e75cd550559be2cf6776530a17
dennxa <denia.98@hotmail.com>
2021-10-28 12:22:37 -0700

    initial commit

--- crud-postgres/inc/settings.php
+++ crud-postgres/inc/settings.php
@@ -8,7 +8,7 @@
 
 //settings heroku
 $servername = "ec2-54-92-230-7.compute-1.amazonaws.com";
-$username = "das6ugnh0snnq8";
+$username = "kwrcwfxtmkswel";
 $password = "2c8b096f70c202ce3d95bd24835a07c4cdbad68eed9be347fa495ab1f7078b13";
 $dbname = "das6ugnh0snnq8";
 $port = 5432;
