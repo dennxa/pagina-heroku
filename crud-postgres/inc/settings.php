1ad80bcf96c73f76fb2312f36f453e248e7d3c61
dennxa <denia.98@hotmail.com>
2021-10-28 12:06:10 -0700

    initial commit

--- crud-postgres/inc/settings.php
+++ crud-postgres/inc/settings.php
@@ -7,10 +7,10 @@
 //$port = 5432;
 
 //settings heroku
-$servername = "localhost";
-$username = "postgres";
-$password = "12345";
-$dbname = "databas";
+$servername = "ec2-18-208-24-104.compute-1.amazonaws.com";
+$username = "qmbtsplubetxjg";
+$password = "f0055bba77fd5f22d770de6119fb88ca75be2985454b001f0d12bc680ca1289d";
+$dbname = "db2bsq925poq2q";
 $port = 5432;
 
 function validar(){
@@ -19,7 +19,7 @@ if (empty($_SESSION["nombre"]))
 {
   echo "Se detecto un acceso ilegal al sistema, su ip esta siendo monitoreada y sera enviada a la policia";
   ?>
-  <a href="http://localhost/crud-postgres">Sitio de login</a>
+  <a href="https://denilomas.herokuapp.com/crud-postgres/">Sitio de login</a>
   <?php
   exit();
 }
